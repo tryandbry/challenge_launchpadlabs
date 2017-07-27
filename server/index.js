f7a3b6a263051ab1617d8e5b71f11cc4c14b4928
bryan <bryan@localhost.localdomain>
2017-07-26 20:29:35 -0500

    changes server port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var {resolve} = require('path');
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(resolve(__dirname,'../pub')));
 app.use(express.static(resolve(__dirname,'../node_modules/jquery/dist')));
 app.use(express.static(resolve(__dirname,'../node_modules/bootstrap/dist/js')));
@@ -9,6 +11,6 @@ app.use(express.static(resolve(__dirname,'../node_modules/bootstrap/dist/css')))
 
 app.use('/api',require('./api'));
 
-app.listen(3000,()=>{
-  console.log('server listening on 3000');
+app.listen(PORT,()=>{
+  console.log('server listening on ' + PORT);
 });
