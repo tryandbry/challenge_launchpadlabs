f15ca6462e00841fdcf8280d0b45c485834b1ec4
bryan <bryan@localhost.localdomain>
2017-07-26 21:49:37 -0500

    update server to handle add routes

--- src/index.js
+++ src/index.js
@@ -9,20 +9,17 @@ import appContainer from './container/appContainer';
 import DashContainer from './container/DashContainer';
 import CommitContainer from './container/CommitContainer';
 import GeneralContainer from './container/GeneralContainer';
+import DetailContainer from './container/DetailContainer';
 
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={appContainer}>
         <IndexRoute component={DashContainer} />
+        <Route path="/detail" component={DetailContainer} />
       </Route>
     </Router>
   </Provider>
   , 
   document.getElementById('main')
 );
-
-
-/*
-        <IndexRoute component={commitContainer} />
-*/
